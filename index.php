15b9191cd4f9b5d94a69bda3bf8205a7c629c874
ShawenHarker <harkershawen@gmail.com>
2023-10-10 19:16:22 +0200

    if else statements

--- index.php
+++ index.php
@@ -3,13 +3,37 @@
     <head>
         <meta charset="UTF-8">
         <title>Php learning path</title>
+        <style>
+            body {
+                display: grid;
+                place-content: center;
+                height: 100vh;
+                margin: 0;
+                font-family: sans-serif;
+            }
+
+            .title {
+                color: blueviolet;
+            }
+            .not {
+                color: red;
+            }
+        </style>
     </head>
     <body>
+        <?php 
+            $bookName = "Rich Dad, Poor Dad";
+            $hasBeenRead = true;
+
+            if ($hasBeenRead) {
+                    $message = "I have read <span class='title'>$bookName</span>";
+                } else {
+                    $message = "I have <span class='not'>NOT</span> read <span class='title'>$bookName</span>";
+                }
+        ?>
         <h1>
-            <?php
-                $greeting = "Hello";
-                echo "$greeting All";
-            ?>
+            <!-- This is the shorthand to <?php echo $message; ?>  -->
+            <?= $message ?>
         </h1>
     </body>
 </html>
