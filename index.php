3566cbd40c0c41bd344c8ba165e276e4391d013c
ShawenHarker <harkershawen@gmail.com>
2023-10-12 10:11:17 +0200

    Adding a syntax that allows for a much cleaner code

--- index.php
+++ index.php
@@ -38,7 +38,6 @@
         <!-- This is the shorthand to <?php echo $message; ?>  -->
         <?= $message ?>
     </h1>
-    <h2>Recommended books</h2>
     <?php
     $listOfBooks = [
         "The 4 hour work week",
@@ -49,12 +48,28 @@
         "Head First JavaScript",
         "Clean Coder"
     ];
+
+    $listOfNames = [
+        "Tim Berners-Lee",
+        "Guido van Rossum",
+        "Brendan Eich",
+        "Dennis Ritchie",
+    ];
     ?>
+    <h2>Recommended books</h2>
     <ul>
         <?php foreach ($listOfBooks as $book) {
             // The code next to books is for the TM symbol, wrapping the variable with {} is a cleaner way to concatenate a string.
             echo "<li>{$book}&#153;</li>";
         } ?>
+    </ul>
+
+    <h2>Recommended Authors</h2>
+    <ul>
+        <!-- This example is the example of writing cleaner code for the foreach loop in php -->
+        <?php foreach ($listOfNames as $name) : ?>
+            <li><?= $name ?></li>
+        <?php endforeach; ?>
     </ul>
 </body>
 
